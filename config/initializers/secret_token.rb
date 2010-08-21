cc5d06fc8fdd8925edb3065e4a547e276a53da54
olivM <olivier.mourlevat@gmail.com>
2010-08-21 10:06:42 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FBGraphJSONtoRSS::Application.config.secret_token = '286323ddcf3710ca5036cd0fbe10016b8161b0adb78635d4ae981ea06fd22ce049121f11d1e2a83e5247573cd48fe7d61e267a35f64832ab092e81a4aa1285f2'
